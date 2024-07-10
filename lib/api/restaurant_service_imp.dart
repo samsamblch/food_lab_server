fbcab84d40b59a7ef3ac90a81d007dc1d3277262
matt2dev <163113717+matt2dev@users.noreply.github.com>
2024-07-10 23:53:15 +0300

    Update restaurant_service_imp.dart

--- lib/api/restaurant_service_imp.dart
+++ lib/api/restaurant_service_imp.dart
@@ -22,7 +22,7 @@ class RestaurantServiceImp extends RestaurantService {
   Future<Response> _restourantHandler(Request req) async {
     final json = await Tools.readJsonFromAsset('assets/jsons/delivery_food/restourant.json');
 
-    await Future.delayed(Duration(seconds: 10));
+    await Future.delayed(Duration(seconds: 3));
 
     return Response.ok(
       jsonEncode(json),
